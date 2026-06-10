5bac5bffc1f2d309c6b2d79ffc7dbb3afc478b34
aalmahmmodi-stack <a.almahmmodi@gmail.com>
2026-06-10 09:35:54 +0200

    Delete drizzle directory

--- drizzle/0002_narrow_beyonder.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `students` DROP INDEX `students_sequentialNumber_unique`;--> statement-breakpoint
-ALTER TABLE `students` DROP INDEX `students_nationalNumber_unique`;--> statement-breakpoint
-ALTER TABLE `students` ADD `isDuplicate` int DEFAULT 0 NOT NULL;
